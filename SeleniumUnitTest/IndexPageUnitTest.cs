da084f84dbfe17111da5135013d37124057b1711
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-08-12 14:59:32 +0530

    Data driven Selenium Unit Test

--- SeleniumUnitTest/IndexPageUnitTest.cs
+++ SeleniumUnitTest/IndexPageUnitTest.cs
@@ -1,6 +1,6 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SeleniumUnitTest {
     [TestClass]
@@ -13,5 +13,18 @@ namespace SeleniumUnitTest {
             indexPage.Login ("username", "passwprd");
             // indexPage.Close ();
         }
+
+        [DataTestMethod]
+        [DataRow ("USerName1", "Password1")]
+        [DataRow ("USerName2", "Password2")]
+        [DataRow ("USerName3", "Password3")]
+        [DataRow ("USerName4", "Password4")]
+        public void DataRowDrivenTestSeleniumMethod (string username, string password) {
+            IWebDriver driver = new ChromeDriver ();
+            driver.Navigate ().GoToUrl ("http://webdriveruniversity.com/Login-Portal/index.html?");
+            IndexPage indexPage = new IndexPage (driver);
+            indexPage.Login (username, password);
+            // indexPage.Close ();
+            }
+        }
     }
-}
